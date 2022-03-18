4fd317d25f5079bed0126c57dbe69415a4ae8453
Divyansh <divyanshvermafast4@gmail.com>
2022-03-18 12:21:06 +0530

    initial

---
+++ main.js
@@ -0,0 +1,35 @@
+
+/*==================== SAVING INPUT ====================*/
+function save() {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let text = document.getElementById("text").value;
+
+    updatingFile(name, email, text);
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("text").value = "";
+}
+
+const config = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+firebase.initializeApp(config);
+
+function updatingFile(name, email, text) {
+  firebase.firestore().collection("messages").add({
+    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    name: name,
+    email: email,
+    message: text,
+  });
+}
